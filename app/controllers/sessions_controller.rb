dec7a09b4b39b00b99b51199bc27d7c5498a0207
Brad Carlson <b2carlso@yahoo.com>
2018-03-09 01:55:21 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+   if !params[:name].blank?
+     session[:name] = params[:name]
+     redirect_to :root
+   else
+     redirect_to action: "new"
+   end
+ end
 end
